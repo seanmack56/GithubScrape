92686dfbc9c0a9f419bb1e3660cbcbde6cffa07f
seanmack56 <39383003+seanmack56@users.noreply.github.com>
2024-12-18 11:35:41 -0400

    added remove duplicates

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("True_Updated_repo_calcs.csv")
-#duplicate_rows = df[df.duplicated(subset=[df.columns[0]])]
+df = pd.read_csv("ADD_CSVFILE_HERE.csv")
 # Identify duplicate rows based on the first column, keeping the first occurrence
 duplicate_rows = df[df.duplicated(subset=[df.columns[0]], keep='first')]
 
 # Remove duplicate rows
 df_no_duplicates = df.drop(duplicate_rows.index)
-df_no_duplicates.to_csv('final_list.csv', index=False)
-
-#duplicate_rows.to_csv("tester3.csv", index=False)
+df_no_duplicates.to_csv('OUTPUTFILE.csv', index=False)
